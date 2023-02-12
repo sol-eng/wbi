5a55cb2c5b152f5be25bf523ff0d8413f5d7672a
Trevor Nederlof <trevor.nederlof@posit.co>
2023-02-12 20:57:14 +0000

    Simplify RunCommand to stream stdout and stderr

--- internal/system/command.go
+++ internal/system/command.go
@@ -1,31 +1,27 @@
 package system
 
 import (
-	"bufio"
 	"fmt"
+	"os"
 	"os/exec"
+	"time"
 )
 
+// Runs a command in the terminal and streams the output
 func RunCommand(command string) error {
 	fmt.Println("Running command: " + command)
+	// sleep for 3 seconds to allow the user to read the command
+	time.Sleep(3 * time.Second)
+
 	cmd := exec.Command("/bin/sh", "-c", command)
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		return err
-	}
-	scanner := bufio.NewScanner(stdout)
-	err = cmd.Start()
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
 	if err != nil {
 		return fmt.Errorf("issue running command: %w", err)
 	}
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-	}
-	if scanner.Err() != nil {
-		cmd.Process.Kill()
-		cmd.Wait()
-		return fmt.Errorf("issue running command: %w", scanner.Err())
-	}
-	cmd.Wait()
+
 	return nil
 }
