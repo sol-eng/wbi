89b6a4c4ae55903e28ca11c722d524e8f6828fc2
Trevor Nederlof <trevor.nederlof@posit.co>
2023-02-12 20:45:24 +0000

    Simplify RunCommand and stream stderr as well

--- internal/system/command.go
+++ internal/system/command.go
@@ -1,8 +1,8 @@
 package system
 
 import (
-	"bufio"
 	"fmt"
+	"os"
 	"os/exec"
 	"time"
 )
@@ -14,23 +14,14 @@ func RunCommand(command string) error {
 	time.Sleep(3 * time.Second)
 
 	cmd := exec.Command("/bin/sh", "-c", command)
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		return err
-	}
-	scanner := bufio.NewScanner(stdout)
-	err = cmd.Start()
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
 	if err != nil {
 		return fmt.Errorf("issue running command: %w", err)
 	}
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-	}
-	if scanner.Err() != nil {
-		cmd.Process.Kill()
-		cmd.Wait()
-		return fmt.Errorf("issue running command: %w", scanner.Err())
-	}
-	cmd.Wait()
+
 	return nil
 }
