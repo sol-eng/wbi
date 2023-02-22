46457bdc17cfa46df75a88582344ebf7fcb7fa84
Trevor Nederlof <trevor.nederlof@posit.co>
2023-02-22 17:38:48 +0000

    Add in check if wbi is running as root and require root

---
+++ internal/os/root.go
@@ -0,0 +1,20 @@
+package os
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/dpastoor/wbi/internal/system"
+)
+
+func CheckIfRunningAsRoot() error {
+	stdout, _, err := system.RunCommandAndCaptureOutput("id -u")
+	if err != nil {
+		return fmt.Errorf("issue running user identification command: %w", err)
+	}
+	if strings.TrimSpace(stdout) != "0" {
+		return errors.New("wbi must be as root. Please run wbi with sudo and try again")
+	}
+	return nil
+}
