6825ad1b8275bb1e2ab34f17c2e996b2f78a2dba
Sam Cofer <47065669+samcofer@users.noreply.github.com>
2023-02-23 19:44:48 -0600

    Fix my bad ToUpper choice

--- internal/languages/install.go
+++ internal/languages/install.go
@@ -33,16 +33,20 @@ func PopulateInstallerInfo(language string, version string, osType config.Operat
 			URL:     "https://cdn.rstudio.com/" + language + "/ubuntu-2204/pkgs/" + language + "-" + version + "_1_amd64.deb",
 			Version: version,
 		}, nil
-	case config.Redhat7: //TODO: Upper the language variable for RHEL
+	case config.Redhat7: //TODO: Upper the R language variable for RHEL
+		if language == "r" {
+			language = strings.ToUpper(language)
+		}
+
 		return InstallerInfo{
 			Name:    language + "-" + version + "-1-1.x86_64.rpm",
-			URL:     "https://cdn.rstudio.com/" + language + "/centos-7/pkgs/" + strings.ToUpper(language) + "-" + version + "-1-1.x86_64.rpm",
+			URL:     "https://cdn.rstudio.com/" + language + "/centos-7/pkgs/" + language + "-" + version + "-1-1.x86_64.rpm",
 			Version: version,
 		}, nil
 	case config.Redhat8:
 		return InstallerInfo{
 			Name:    language + "-" + version + "-1-1.x86_64.rpm",
-			URL:     "https://cdn.rstudio.com/" + language + "/centos-8/pkgs/" + strings.ToUpper(language) + "-" + version + "-1-1.x86_64.rpm",
+			URL:     "https://cdn.rstudio.com/" + language + "/centos-8/pkgs/" + language + "-" + version + "-1-1.x86_64.rpm",
 			Version: version,
 		}, nil
 	default:
