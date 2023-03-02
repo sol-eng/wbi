c751334d4e84d227c19c06f821e294b1fe9c19ff
Trevor Nederlof <trevor.nederlof@posit.co>
2023-03-02 13:40:13 +0000

    Switch fury to wbi user

--- scripts/fury-upload.sh
+++ scripts/fury-upload.sh
@@ -3,7 +3,7 @@ set -e
 if [ "${1: -4}" == ".deb" ] || [ "${1: -4}" == ".rpm" ]; then
 	cd dist
 	echo "uploading $1"
-	status="$(curl -s -q -o /dev/null -w "%{http_code}" -F package="@$1" "https://$FURY_TOKEN@push.fury.io/tnederlof/")"
+	status="$(curl -s -q -o /dev/null -w "%{http_code}" -F package="@$1" "https://$FURY_TOKEN@push.fury.io/wbi/")"
 	echo "got: $status"
 	if [ "$status" == "200" ] || [ "$status" == "409" ]; then
 		exit 0
