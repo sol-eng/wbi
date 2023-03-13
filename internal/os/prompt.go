6f5829eabae99ba22264e6c08f2c34d5eb4e9f4d
Sam Cofer <47065669+samcofer@users.noreply.github.com>
2023-03-13 12:30:05 -0500

    Change variables for clarity

--- internal/os/prompt.go
+++ internal/os/prompt.go
@@ -9,10 +9,10 @@ import (
 func FirewallPrompt() (bool, error) {
 	name := true
 	prompt := &survey.Confirm{
-		Message: "Posit products are often blocked by local server firewalls, most organizations " +
-			"do not rely on local firewalls for server security. If your organization controls access " +
-			"to this server with an external firewall, we recommend disabling the local firewall. Would you like " +
-			"to disable the local firewall?",
+		Message: "Posit products are often blocked by local server firewalls, most organizations\n " +
+			"do not rely on local firewalls for server security. If your organization controls access\n " +
+			"to this server with an external firewall, we recommend disabling the local firewall.\n" +
+			" Would you like to disable the local firewall?",
 	}
 	err := survey.AskOne(prompt, &name)
 	if err != nil {
@@ -26,8 +26,8 @@ func SELinuxPrompt(osType config.OperatingSystem) (bool, error) {
 	if osType == config.Redhat7 || osType == config.Redhat8 {
 		name = true
 		prompt := &survey.Confirm{
-			Message: "SELinux is often enabled by default on Redhat Linux distributions. We recommend that SELinux be" +
-				"disabled, unless you and your organization have specific security requirements that require its use." +
+			Message: "SELinux is often enabled by default on Redhat Linux distributions. \nWe recommend that SELinux be" +
+				"disabled, unless you and your organization have \nspecific security requirements that require its use.\n" +
 				"Would you like to disable SELinux on this server?",
 		}
 		err := survey.AskOne(prompt, &name)
