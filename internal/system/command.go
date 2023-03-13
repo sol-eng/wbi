cdb5e11b5177075a8d80ab34f6ea89c2b6b3e9c9
Sam Cofer <47065669+samcofer@users.noreply.github.com>
2023-03-13 16:34:24 -0500

    Try Command.Output

--- internal/system/command.go
+++ internal/system/command.go
@@ -31,8 +31,7 @@ func RunCommand(command string) error {
 func RunCommandAndCaptureOutput(command string) (string, string, error) {
 	fmt.Println("Running command for output: " + command)
 
-	//testcommand := "/bin/sh -c \"" + command + "\""
-	cmd := exec.Command(command)
+	cmd := exec.Command("/bin/sh", "-c "+command)
 
 	var errb bytes.Buffer
 	//cmd.Stdout = &outb
