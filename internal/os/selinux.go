1296f43c906d3248af68d5cbca1310bd5317d958
Sam Cofer <47065669+samcofer@users.noreply.github.com>
2023-03-14 16:20:18 -0500

    Nicer messagings around SELinux check.

--- internal/os/selinux.go
+++ internal/os/selinux.go
@@ -10,10 +10,12 @@ import (
 
 func CheckSELinuxStatus(osType config.OperatingSystem) (bool, error) {
 	if osType == config.Redhat7 || osType == config.Redhat8 {
+		fmt.Println("Checking to see if SELinux is active on this server")
 		stdout, _, err := system.RunCommandAndCaptureOutput("getenforce")
 		if err != nil {
 			return false, fmt.Errorf("issue running getenforce command: %w", err)
 		}
+		fmt.Println("SELinux Status: " + stdout)
 		if strings.Contains(stdout, "Enforcing") {
 			return true, nil
 		}
