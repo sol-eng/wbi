301d33181b752046b1217166cffd0fa80fd4f784
Sam Cofer <47065669+samcofer@users.noreply.github.com>
2023-03-14 15:50:57 -0500

    Don't reinstall EPEL on RHEL7 if it is already installed

--- internal/os/install.go
+++ internal/os/install.go
@@ -129,11 +129,10 @@ func EnableEPELRepo(osType config.OperatingSystem) error {
 	if err != nil {
 		return fmt.Errorf("issue retrieving EPEL install command: %w", err)
 	}
-	err = system.RunCommand(EPELCommand)
+	stdout, _, err := system.RunCommandAndCaptureOutput(EPELCommand)
 	if err != nil {
-		if strings.Contains(err.Error(), "exit status 1") && osType == config.Redhat7 {
-			EPELUpdateCommand := "yum reinstall -y " + EPELURL
-			err = system.RunCommand(EPELUpdateCommand)
+		if strings.Contains(stdout, "does not update installed package") && osType == config.Redhat7 {
+			err = nil
 		}
 	}
 	if err != nil {
