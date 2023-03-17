cf848d500f3819b8741da47d2a7bfa19c424ceb2
Sam Cofer <samcofer@gmail.com>
2023-03-17 09:33:19 -0500

    Add requirements and make it look nice!

--- internal/os/prompt.go
+++ internal/os/prompt.go
@@ -21,7 +21,14 @@ func PromptCloud(osType config.OperatingSystem) (bool, error) {
 func PromptInstallPrereqs() (bool, error) {
 	name := false
 	prompt := &survey.Confirm{
-		Message: "Please confirm that you're ready to install Workbench.",
+		Message: "In order to install Workbench from start to finish, you will need the following things\n" +
+			"1. Internet access for this server\n" +
+			"2. Versions of R and Python you would like to install\n" +
+			"3. Your Workbench license key string in this form: XXXX-XXXX-XXXX-XXXX-XXXX-XXXX-XXXX\n" +
+			"4. The location on this server of your SSL key and certificate files (optional)\n" +
+			"5. The URL for your instance of Posit Package Manager (optional)\n" +
+			"6. The URL for your instance of Posit Connect (optional)\n\n" +
+			"Please confirm that you're ready to install Workbench.",
 	}
 	err := survey.AskOne(prompt, &name)
 	if err != nil {
