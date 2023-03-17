4c24aa3525abcb1e025fc759bf46bba8ce2832f2
Trevor Nederlof <trevor.nederlof@posit.co>
2023-03-17 19:47:45 +0000

    Add additional info messages to status active

--- internal/workbench/status.go
+++ internal/workbench/status.go
@@ -25,7 +25,8 @@ func StatusRStudioServer() error {
 		return fmt.Errorf("issue running status for rstudio-server: %w", err)
 	}
 	if strings.Contains(status, "active (running)") {
-		fmt.Println("\nrstudio-server status is active (running)!\n")
+		fmt.Println("\nrstudio-server status is active (running)! See details below:\n")
+		fmt.Println(status)
 	} else {
 		fmt.Println("\nrstudio-server status is inactive (dead)! See details below:\n")
 		fmt.Println(status)
@@ -40,7 +41,8 @@ func StatusRStudioLauncher() error {
 		return fmt.Errorf("issue running status for rstudio-launcher: %w", err)
 	}
 	if strings.Contains(status, "active (running)") {
-		fmt.Println("\nrstudio-launcher status is active (running)!\n")
+		fmt.Println("\nrstudio-launcher status is active (running)! See details below:\n")
+		fmt.Println(status)
 	} else {
 		fmt.Println("\nrstudio-launcher status is inactive (dead)! See details below:\n")
 		fmt.Println(status)
