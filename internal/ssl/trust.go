ab929e10be5cf65b800cd78bdc31a46ab0568ca0
Sam Cofer <samcofer@gmail.com>
2023-04-28 12:15:09 -0500

    Switch to system.WriteStrings

--- internal/ssl/trust.go
+++ internal/ssl/trust.go
@@ -6,17 +6,16 @@ import (
 	"fmt"
 	"github.com/sol-eng/wbi/internal/config"
 	"github.com/sol-eng/wbi/internal/system"
-	"os"
 )
 
 func TrustRootCertificate(rootCA *x509.Certificate, osType config.OperatingSystem) error {
 	fmt.Println(rootCA.Raw)
 	block := pem.Block{Type: "CERTIFICATE", Bytes: rootCA.Raw}
-	pemCert := pem.EncodeToMemory(&block)
-
+	var pemCert []string
+	pemCert = append(pemCert, string(pem.EncodeToMemory(&block)))
 	switch osType {
 	case config.Ubuntu18, config.Ubuntu20, config.Ubuntu22:
-		err := os.WriteFile("/usr/local/share/ca-certificates/workbenchCA.crt", pemCert, 0755)
+		err := system.WriteStrings(pemCert, "/usr/local/share/ca-certificates/workbenchCA.crt", 0755)
 		if err != nil {
 			return fmt.Errorf("writing certificate to disk failed: ", err)
 		}
@@ -25,7 +24,7 @@ func TrustRootCertificate(rootCA *x509.Certificate, osType config.OperatingSyste
 			return fmt.Errorf("running command to trust root certificate: ", err)
 		}
 	case config.Redhat7, config.Redhat8, config.Redhat9:
-		err := os.WriteFile("/etc/pki/ca-trust/source/anchors/workbenchCA.crt", pemCert, 0755)
+		err := system.WriteStrings(pemCert, "/etc/pki/ca-trust/source/anchors/workbenchCA.crt", 0755)
 		if err != nil {
 			return fmt.Errorf("writing CA certificate to disk failed: ", err)
 		}
