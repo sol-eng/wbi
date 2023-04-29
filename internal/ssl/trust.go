446154e34629f58097854adec62a8cf28644601b
Sam Cofer <samcofer@gmail.com>
2023-04-28 20:46:20 -0500

    Fixing errors

--- internal/ssl/trust.go
+++ internal/ssl/trust.go
@@ -16,20 +16,20 @@ func TrustRootCertificate(rootCA *x509.Certificate, osType config.OperatingSyste
 	case config.Ubuntu18, config.Ubuntu20, config.Ubuntu22:
 		err := system.WriteStrings(pemCert, "/usr/local/share/ca-certificates/workbenchCA.crt", 0755)
 		if err != nil {
-			return fmt.Errorf("writing certificate to disk failed: ", err)
+			return fmt.Errorf("writing certificate to disk failed: %w", err)
 		}
 		err = system.RunCommand("update-ca-certificates", true, 1)
 		if err != nil {
-			return fmt.Errorf("running command to trust root certificate: ", err)
+			return fmt.Errorf("running command to trust root certificate: %w", err)
 		}
 	case config.Redhat7, config.Redhat8, config.Redhat9:
 		err := system.WriteStrings(pemCert, "/etc/pki/ca-trust/source/anchors/workbenchCA.crt", 0755)
 		if err != nil {
-			return fmt.Errorf("writing CA certificate to disk failed: ", err)
+			return fmt.Errorf("writing CA certificate to disk failed: %w", err)
 		}
 		err = system.RunCommand("update-ca-trust", true, 1)
 		if err != nil {
-			return fmt.Errorf("running command to trust root certificate: ", err)
+			return fmt.Errorf("running command to trust root certificate: %w", err)
 		}
 	}
 	return nil
