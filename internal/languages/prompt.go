04b6ab1b65300dddcb3ad2de33afc9db0d5c9ba8
Trevor Nederlof <trevor.nederlof@posit.co>
2023-08-10 20:11:24 +0000

    Convert languages prompt to pterm

--- internal/languages/prompt.go
+++ internal/languages/prompt.go
@@ -4,36 +4,31 @@ import (
 	"errors"
 	"strings"
 
-	"github.com/AlecAivazis/survey/v2"
+	"github.com/pterm/pterm"
 	"github.com/samber/lo"
 	log "github.com/sirupsen/logrus"
 )
 
 // Prompt asking users which languages they will use
 func PromptAndRespond() ([]string, error) {
-	messageText := "What languages will you use"
-	var qs = []*survey.Question{
-		{
-			Name: "languages",
-			Prompt: &survey.MultiSelect{
-				Message: messageText,
-				Options: []string{"R", "python"},
-				Default: []string{"R", "python"},
-			},
-		},
-	}
-	languageAnswers := struct {
-		Languages []string `survey:"languages"`
-	}{}
+	promptText := "What languages will you use"
 
-	err := survey.Ask(qs, &languageAnswers, survey.WithRemoveSelectAll(), survey.WithRemoveSelectNone())
+	selectOptions := []string{"R", "python"}
+
+	result, err := pterm.DefaultInteractiveMultiselect.
+		WithDefaultText(promptText).
+		WithDefaultOptions(selectOptions).
+		WithOptions(selectOptions).
+		WithFilter(false).
+		Show()
 	if err != nil {
 		return []string{}, errors.New("there was an issue with the languages prompt")
 	}
-	if !lo.Contains(languageAnswers.Languages, "R") {
+	if !lo.Contains(result, "R") {
 		return []string{}, errors.New("R must be a select language to install Workbench")
 	}
-	log.Info(messageText)
-	log.Info(strings.Join(languageAnswers.Languages, ", "))
-	return languageAnswers.Languages, nil
+
+	log.Info(promptText)
+	log.Info(strings.Join(result, ", "))
+	return result, nil
 }
